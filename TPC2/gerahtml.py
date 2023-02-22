39c951c27e299e9582d426a0ae900baff6d6b41e
Millena Freitas <59456180+freitasmillena@users.noreply.github.com>
2023-02-22 17:30:57 +0000

    inclui distritos

--- TPC2/gerahtml.py
+++ TPC2/gerahtml.py
@@ -1,30 +1,37 @@
 import json
-import os
+
 
 def ordCidade(cidade):
     return cidade['nome']
+
+def ordDistrito(distrito):
+    return distrito
 
 f = open("mapa.json")
 mapa = json.load(f)
 cidades = mapa['cidades']
 cidades.sort(key=ordCidade)
 locations = mapa['ligações']
-
+distritos = {} #chave - distrito, valor - id da cidade
 mapCidades = {}
+
+
 
 mapLocations = {}
 
 for c in cidades:
-    mapCidades[c['id']] = c['nome']
+    mapCidades[c['id']] = c
+    if c['distrito'] not in distritos:
+        distritos[c['distrito']] = []
+    distritos[c['distrito']].append(c['id'])
 
 for l in locations:
     if l['origem'] not in mapLocations:
         mapLocations[l['origem']] = []
     mapLocations[l['origem']].append(l)
 
-  
-print(cidades)
-
+distritoslist = list(distritos.keys())
+distritoslist.sort()
 
 
 pagHTML = """
@@ -38,12 +45,18 @@ pagHTML = """
         <h1>Mapa Virtual</h1>
             <!--Índice-->
             <a name="indice"/>
-            <h3>Cidades</h3>
-             <ol>
+            <h2>Distritos</h2>
+             
 """
 
-for c in cidades:
-    pagHTML += f"<li><a href='{c['id']}'>{c['nome']}</a></li>"
+
+for d in distritoslist:
+    pagHTML += f"""
+    <h3><b>{d}</b></h3>
+    """
+    for c in distritos[d]:
+        dados = mapCidades[c]
+        pagHTML += f"<li><a href='{dados['id']}'>{dados['nome']}</a></li>"
 
 pagHTML += """
 </ol>
@@ -91,7 +104,3 @@ for c in cidades:
     file = open(c['id']+".html", "w")
     file.write(cidadeHTML)
     file.close()
-
-
-
-
